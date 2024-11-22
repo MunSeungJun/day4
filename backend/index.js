b6ae004f459c48592d7606b9a9b83f6dfa82bd55
MunSeungJun <sbs2469@naver.com>
2024-11-22 14:59:50 +0900

    server inspection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
-const express = require('express')
+import express from "express"
+import path from "path"
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Hello World!')
+  res.sendFile(path.join(__dirname, "notice.html"))
 })
 
 app.listen(port, () => {
